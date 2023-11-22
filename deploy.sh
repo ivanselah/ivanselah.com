3f3d2736da370b58cd5799c0a4b7d40c2d943ceb
ivanselah <tft0720@gmail.com>
2023-11-23 08:29:08 +0900

    pm2 심볼릭 링크 연결

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 REPOSITORY=/home/ubuntu/ivanselah-com-bucket-deploy
 
+which pm2
+sudo ln -s $(which pm2) /usr/bin/pm2
+sudo ln -s $(which pm2) /usr/local/bin/pm2
+
 cd $REPOSITORY
 pm2 start --name "ivanselah" yarn -- start -- --port 3060
