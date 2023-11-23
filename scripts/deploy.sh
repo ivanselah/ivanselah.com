c9fd34bba49a6544cac0c2000c4cdda2006ff55f
ivanselah <tft0720@gmail.com>
2023-11-23 11:41:02 +0900

    pm2 npx 실행

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,8 @@ $YARN_PATH install --ignore-engines
 if pm2 list | grep -q "next-server"; then
    echo "PM2 is already running"
 else
-   $PM2_PATH start yarn --name next-server -- start
+   npx pm2 start --name next-server yarn -- start
+ # $PM2_PATH start yarn --name next-server -- start
 fi
 
 
