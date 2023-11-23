609744771b440480e91628fdafed0b7f82863206
ivanselah <tft0720@gmail.com>
2023-11-23 10:26:25 +0900

    pm2 명령어 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 REPOSITORY=/home/ubuntu/ivanselah-com-bucket-deploy
 
 cd $REPOSITORY
-pm2 start npx --name "next-server" -- next -p 3060
+pm2 start yarn --name "next-server" --interpreter bash -- start
