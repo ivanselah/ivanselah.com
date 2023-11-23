3be2592c791089525adb75e5db33e1a9628e81e7
ivanselah <tft0720@gmail.com>
2023-11-23 12:02:09 +0900

    last

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,19 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 REPOSITORY=/home/ubuntu/ivanselah-com-bucket-deploy
 
 cd $REPOSITORY
 
 YARN_PATH=$(which yarn)
-PM2_PATH=$(which pm2)
 $YARN_PATH install --ignore-engines
 
 if pm2 list | grep -q "next-server"; then
    echo "PM2 is already running"
 else
-   npx pm2 start --name next-server yarn -- start
- # $PM2_PATH start yarn --name next-server -- start
+   # npx pm2 start --name next-server yarn -- start
+   pm2 start yarn --name next-server -- start
 fi
 
 
