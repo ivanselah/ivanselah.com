c9ab666b93f179f98e5c72e85e0f889668f29152
ivanselah <tft0720@gmail.com>
2023-12-20 13:25:38 +0900

    feat: markdown 스타일 변경 및 포스트 업데이트

--- src/components/posts/MarkdownViewer.tsx
+++ src/components/posts/MarkdownViewer.tsx
@@ -18,12 +18,36 @@ export default function MarkdownViewer({ content }: MarkdownViewerProps) {
       className="prose max-w-none dark:text-neutral-100"
       remarkPlugins={[remarkGfm]}
       components={{
-        h1: ({ children }) => <h1 className="dark:text-neutral-200">{children}</h1>,
-        h2: ({ children }) => <h2 className="dark:text-neutral-200">{children}</h2>,
-        h3: ({ children }) => <h3 className="dark:text-neutral-200">{children}</h3>,
+        h1: ({ children }) => (
+          <h1 className="relative w-fit dark:text-neutral-200 max-md:text-lg after:absolute after:bg-sky-500 after:inline-block after:w-3 after:h-3 after:rounded-full after:bottom-0 after:-right-4 max-md:after:bottom-1 max-md:after:-right-3 max-md:after:w-2 max-md:after:h-2">
+            {children}
+          </h1>
+        ),
+        h2: ({ children }) => (
+          <h2 className="relative w-fit dark:text-neutral-200 max-md:text-base after:absolute after:bg-sky-500 after:inline-block after:w-2 after:h-2 after:rounded-full after:bottom-1 after:-right-3 max-md:after:-right-2 max-md:after:w-1.5 max-md:after:h-1.5">
+            {children}
+          </h2>
+        ),
+        h3: ({ children }) => (
+          <h3 className="dark:text-neutral-200  max-md:text-sm border-l-4 pl-2 border-l-teal-300">{children}</h3>
+        ),
         h4: ({ children }) => <h4 className="dark:text-neutral-200">{children}</h4>,
         li: ({ children }) => <li className="dark:text-neutral-200 max-md:text-sm">{children}</li>,
-        p: ({ children }) => <p className="dark:text-neutral-200 max-md:text-sm">{children}</p>,
+        em: ({ children }) => <em className="dark:text-neutral-200 max-md:text-sm">{children}</em>,
+        strong: ({ children }) => (
+          <strong className="text-rose-500 dark:text-teal-500 max-md:text-sm">{children}</strong>
+        ),
+        blockquote: ({ children }) => (
+          <blockquote className="bg-neutral-50 dark:bg-neutral-900 px-7 py-2 border-l-sky-500 rounded-md not-italic font-light">
+            {children}
+          </blockquote>
+        ),
+        p: ({ children }) => (
+          <p className="dark:text-neutral-200 max-md:text-sm whitespace-pre-wrap font-light before:hidden">
+            {children}
+          </p>
+        ),
+        a: ({ children }) => <a className="text-neutral-500 max-md:text-sm cursor-pointer">{children}</a>,
         code(props) {
           const { children, className, node, ...rest } = props;
           const match = /language-(\w+)/.exec(className || '');
@@ -37,7 +61,7 @@ export default function MarkdownViewer({ content }: MarkdownViewerProps) {
             </code>
           );
         },
-        img: image => <Image src={image.src || ''} alt={image.alt || ''} width={500} height={350} />,
+        img: image => <Image className="w-full" src={image.src || ''} alt={image.alt || ''} width={500} height={350} />,
       }}
     >
       {content}
